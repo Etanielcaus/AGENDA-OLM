94f3705bbb568341004111289dfab5b9c20f4f9d
Etanielcaus <nielfilosofia@gmail.com>
2023-10-04 06:42:10 -0300

    criação do primeiro model e já enviado para a DB

--- contact/models.py
+++ contact/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 # id (primary key - automático)
@@ -9,3 +10,20 @@ from django.db import models
 # category (foreign key), show (boolean), owner (foreign key)
 # picture (imagem)
 
+# Aqui, você cria uma classe para os campos serem preenchidos para a
+# base de dados. O Django cria o id automaticamente.
+# A parte de Field, define o que será cada categoria.
+# CharField = String, EmailField = Email, DateField = Data, TextField = Texto
+# DateTimeField = Data e Hora, BooleanField = Booleano (True/False)
+# para tornar opcional, você precisa colocar blank=True
+# Para criar a base de dados, você precisa rodar o comando:
+# python manage.py makemigrations
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)  # pegar a data
+    description = models.TextField(blank=True)
