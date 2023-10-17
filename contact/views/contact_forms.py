510d795fd83f237940ad8da450f53b7f199122ab
Etanielcaus <nielfilosofia@gmail.com>
2023-10-17 06:40:49 -0300

    configurando o method

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,15 +1,26 @@
-from django.shortcuts import get_object_or_404, render, redirect
 from django.core.paginator import Paginator
 from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
 from contact.models import Contact
 
 
 def create(request):
+    post = request.POST
+    if request.method == 'POST':
+        print()
+        print(request.method)
+        print(request.POST.get('first_name'))
+        print(request.POST.get('last_name'))
+        print()
+
     context = {
 
     }
 
-    # renderizando o template
+    print()
+    print(request.method)
+    print()
+
     return render(
         request,
         'contact/create.html',
