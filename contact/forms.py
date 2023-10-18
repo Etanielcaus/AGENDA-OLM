d735344c7eb11e5d8552dae9cecba76723674cdd
Etanielcaus <nielfilosofia@gmail.com>
2023-10-18 07:01:38 -0300

    criando formulario

--- contact/forms.py
+++ contact/forms.py
@@ -3,11 +3,48 @@ from django.core.exceptions import ValidationError
 from . import models
 
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'classe-a classe-b',
+                'placeholder': 'Escreva aqui'
+            }
+        ),
+        label='Primeiro Nome',
+        help_text='Texto de ajuda para usuário',
+    )
+
+    qualquer = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'classe-a classe-b',
+                'placeholder': 'Escreva aqui'
+            }
+        ),
+        help_text='Texto de ajuda para usuário',
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # self.fields['first_name'].widget.attrs.update({
+        #     'class': 'classe-a classe-b',
+        #     'placeholder': 'Escreva aqui'
+        # })
+    
     class Meta:
         model = models.Contact
         fields = (
             'first_name', 'last_name', 'phone',
         )
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs={
+        #             'class': 'classe-a classe-b',
+        #             'placeholder': 'Escreva aqui'
+        #         }
+        #     )
+        # }
 
     def clean(self):
         cleaned_data = self.cleaned_data
